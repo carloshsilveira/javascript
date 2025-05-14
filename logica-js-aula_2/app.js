e327635fcc2b7f572eb51a22c9058f3b2b69d3d1
Carlos Silveira <onecarlos36student@gmail.com>
2025-05-14 15:03:59 -0300

    commit

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,11 +1,21 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+// alert('Boas vindas ao jogo do número secreto');
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+let numeroSecreto = 4;
+
+let chute;
+
+let tentativas = 0;
+
+while (chute != numeroSecreto) {
+    tentativas++;
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert(`Descobriu o número secreto ${numeroSecreto}. Você fez ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 }
